54dd27e54e7d3782e981d431f6896c0968c773c1
Nicholas Gabriel <109010700+Inicbrz@users.noreply.github.com>
2023-03-28 19:30:05 -0300

    Trabalho Humberto.

---
+++ script.js
@@ -0,0 +1,82 @@
+const botaoum = document.querySelector('.botaoum');
+const slideum = document.querySelector('.slide1');
+
+botaoum.addEventListener('click', ()=> {
+    slideum.classList.add('active');
+});
+
+
+const botaodois = document.querySelector('.botaodois');
+const slidedois = document.querySelector('.slide2');
+
+botaodois.addEventListener('click', ()=> {
+    slidedois.classList.add('active');
+});
+
+
+
+const botaotres = document.querySelector('.botaotres');
+const slidetres = document.querySelector('.slide3');
+
+botaotres.addEventListener('click', ()=> {
+    slidetres.classList.add('active');
+});
+
+
+
+const botaoquatro = document.querySelector('.botaoquatro');
+const slidequatro = document.querySelector('.slide4');
+
+botaoquatro.addEventListener('click', ()=> {
+    slidequatro.classList.add('active');
+});
+
+
+
+const botaocinco = document.querySelector('.botaocinco');
+const slidecinco = document.querySelector('.slide5');
+
+botaocinco.addEventListener('click', ()=> {
+    slidecinco.classList.add('active');
+});
+
+
+const imgum = document.querySelector('.imgum');
+const askimgum = document.querySelector('.askimgum');
+
+imgum.addEventListener('click', ()=> {
+    askimgum.classList.toggle('active');
+});
+
+
+const imgdois = document.querySelector('.imgdois');
+const askimgdois = document.querySelector('.askimgdois');
+
+imgdois.addEventListener('click', ()=> {
+    askimgdois.classList.toggle('active');
+});
+
+const imgtres = document.querySelector('.imgtres');
+const askimgtres = document.querySelector('.askimgtres');
+
+imgtres.addEventListener('click', ()=> {
+    askimgtres.classList.toggle('active');
+});
+
+
+
+
+const botaoseis = document.querySelector('.botaoseis');
+const slideseis = document.querySelector('.slide6');
+
+botaoseis.addEventListener('click', ()=> {
+    slideseis.classList.toggle('active');
+});
+
+
+const botaosete = document.querySelector('.botaosete');
+const slidesete = document.querySelector('.slide7');
+
+botaosete.addEventListener('click', ()=> {
+    slidesete.classList.toggle('active');
+});
